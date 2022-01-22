f12259f6e4452be88b9a163c36e78504e18e3be5
theo galais <theogalais@gmail.com>
2022-01-22 09:44:56 +0100

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts 'Creating 5 fake flats...'
+4.times do
+  flat = Flat.new(
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Lorem.sentence,
+    price_per_night: Faker::Number.between(from: 40, to: 200),
+    number_of_guests: Faker::Number.between(from: 1, to: 4)
+  )
+  flat.save!
+end
+puts 'Finished!'
